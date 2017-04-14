654be76334e22f385369f7c7259fa278e6bd3ad3
Thomas <thomas@tphil.nl>
2017-04-14 13:56:36 +0200

    tussendoor commit

--- strongr/saltmaster/commandhandlers/cloud/opennebula/deployvmhandler.py
+++ strongr/saltmaster/commandhandlers/cloud/opennebula/deployvmhandler.py
@@ -8,7 +8,7 @@ class DeployVmHandler():
         if command.ram > 0: kwargs['memory'] = command.ram * 1024
         if command.cores > 0: kwargs['cpu'] = command.cores
 
-        command.name = re.sub(r'[^a-zA-Z0-9]*', '', command.name)
+        command.name = re.sub('[^a-zA-Z0-9]*', '', str(command.name))
 
         client = salt.cloud.CloudClient('/etc/salt/cloud')
         vm = client.profile(names=[command.name], profile='salt-minion', **kwargs)
