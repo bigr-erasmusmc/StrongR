5ca787ba942c68f9926efc30073d9fa71f1fc0d1
Thomas <thomas@tphil.nl>
2017-04-14 16:01:13 +0200

    tussendoor commit

--- strongr/master/commandhandlers/cloud/opennebula/deployvmhandler.py
+++ strongr/master/commandhandlers/cloud/opennebula/deployvmhandler.py
@@ -6,7 +6,10 @@ class DeployVmHandler():
         overrides = {}
 
         if command.ram > 0: overrides['memory'] = command.ram * 1024
-        if command.cores > 0: overrides['cpu'] = command.cores
+
+        if command.cores > 0: 
+            overrides['cpu'] = command.cores
+            overrides['vcpu'] = command.cores
 
         client = salt.cloud.CloudClient('/etc/salt/cloud')
         return client.profile(names=[command.name], profile='salt-minion', vm_overrides=overrides)
