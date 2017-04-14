d9eb99bb4df41a3445eb58e13fd6ee6b6a8f2c07
Thomas <thomas@tphil.nl>
2017-04-14 09:54:47 +0200

    tussendoor commit

--- strongr/saltmaster/app.py
+++ strongr/saltmaster/app.py
@@ -1,12 +1,8 @@
 from cleo import Application
 
-#from cli import ConfigHelperCommand
-#from cli import OpenNebulaConfigHelper
 from cli import DeployOpenNebulaCommand
 
 application = Application()
-#application.add(ConfigHelperCommand(application))
-#application.add(OpenNebulaConfigHelper())
 application.add(DeployOpenNebulaCommand())
 
 if __name__ == '__main__':
