e485fc07b03a7e193cfa384c475342de91c5a4dc
Thomas <thomas@tphil.nl>
2017-05-30 14:25:08 +0200

    tussendoorcommitje

--- strongr/clouddomain/query/listdeployedvms.py
+++ strongr/clouddomain/query/listdeployedvms.py
@@ -1,2 +1,11 @@
 class ListDeployedVms:
-    pass
+    def __call__(self, command):
+        client = salt.cloud.CloudClient('/etc/salt/cloud')
+        names = []
+        rs = client.query()
+
+        for provider in list(rs.keys()):
+            for location in list(rs[provider].keys()):
+                names.extend(list(rs[provider][location].keys()))
+        return names
+
