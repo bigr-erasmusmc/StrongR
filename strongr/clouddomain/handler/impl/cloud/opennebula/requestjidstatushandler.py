4ce9ed8c1f9bca879037444908a203e03ae2a6df
Thomas <thomas@tphil.nl>
2017-05-31 16:08:12 +0200

    tussendoorcommitje

--- strongr/clouddomain/handler/impl/cloud/opennebula/requestjidstatushandler.py
+++ strongr/clouddomain/handler/impl/cloud/opennebula/requestjidstatushandler.py
@@ -1,9 +1,9 @@
 from strongr.clouddomain.handler.abstract.cloud import AbstractRequestJidStatusHandler
 
-import salt.client
+import salt.runner
 
 class RequestJidStatusHandler(AbstractRequestJidStatusHandler):
     def __call__(self, query):
-        runner = salt.client.RunnerClient()
+        runner = salt.runner.RunnerClient()
         result = runner.cmd('jobs.lookup_jid', query.jid)
         return result
