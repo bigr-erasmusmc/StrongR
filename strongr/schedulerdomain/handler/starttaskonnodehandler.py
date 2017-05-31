3456da39c1d3188b0af8ed0cfb84252727ae7730
Thomas <thomas@tphil.nl>
2017-05-31 14:54:35 +0200

    tussendoorcommitje

--- strongr/schedulerdomain/handler/starttaskonnodehandler.py
+++ strongr/schedulerdomain/handler/starttaskonnodehandler.py
@@ -13,4 +13,4 @@ class StartTaskOnNodeHandler:
         taskinfo = queryBus.handle(queryFactory.newRequestTaskInfo(command.taskid))
 
         jid = cloudCommandBus.handle(cloudCommandFactory.newRunShellCodeCommand(sh=taskinfo["cmd"], host=command.node))
-        core.cache().set("jidmap." + taskinfo["taskid"], jid, '3600')
+        core.cache().set("jidmap." + taskinfo["taskid"], jid, 3600)
