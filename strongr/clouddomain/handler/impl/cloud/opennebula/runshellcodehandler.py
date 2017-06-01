f582f8ed491c8762929ac17dc4c007548bf9c978
Thomas <thomas@tphil.nl>
2017-06-01 09:42:25 +0200

    tussendoorcommitje

--- strongr/clouddomain/handler/impl/cloud/opennebula/runshellcodehandler.py
+++ strongr/clouddomain/handler/impl/cloud/opennebula/runshellcodehandler.py
@@ -5,6 +5,5 @@ import salt.client
 class RunShellCodeHandler(AbstractRunShellCodeHandler):
     def __call__(self, command):
         local = salt.client.LocalClient()
-        #result = local.cmd_async(command.host, 'cmd.run', [command.sh, "runas=ubuntu"])
-        result = local.cmd_async(command.host, 'cmd.run', ['whoami', "runas=ubuntu"])
+        result = local.cmd_async(command.host, 'cmd.run', [command.sh, "runas=ubuntu"])
         return result
