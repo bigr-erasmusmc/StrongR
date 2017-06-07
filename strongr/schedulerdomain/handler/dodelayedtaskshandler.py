7aef3cabd6ed766bfef42e2283f3ad37b94db3b4
Thomas <thomas@tphil.nl>
2017-06-07 13:38:57 +0200

    wasd

--- strongr/schedulerdomain/handler/dodelayedtaskshandler.py
+++ strongr/schedulerdomain/handler/dodelayedtaskshandler.py
@@ -1,11 +1,10 @@
-import filelock
-
 import strongr.core
 
 class DoDelayedTasksHandler:
     def __call__(self, command):
         core = strongr.core.getCore()
         schedulerService = core.domains().schedulerDomain().schedulerService()
+
         queryBus = schedulerService.getQueryBus()
         queryFactory = core.domains().schedulerDomain().queryFactory()
 
@@ -24,8 +23,10 @@ class DoDelayedTasksHandler:
             if taskinfo == None: # this should be an exception at some point
                 continue
             if taskinfo["taskid"] in runningTasks and runningTasks[taskinfo["taskid"]]:
+                # check if task is running and do some updates
                 commandBus.handle(commandFactory.newCheckTaskRunning(taskinfo["taskid"]))
             else:
+                # task is not running or finished, let's try to execute it on an available node
                 node = queryBus.handle(queryFactory.newFindNodeWithAvailableResources(taskinfo["cores"], taskinfo["ram"]))
                 if node == None: # this should be an exception at some point
                     continue
