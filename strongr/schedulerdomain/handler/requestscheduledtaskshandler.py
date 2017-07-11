25ac2a312915203495149cddd9988819377c32f2
Thomas <thomas@tphil.nl>
2017-07-11 13:53:39 +0200

    bugfixje wanneer er geen taken zijn

--- strongr/schedulerdomain/handler/requestscheduledtaskshandler.py
+++ strongr/schedulerdomain/handler/requestscheduledtaskshandler.py
@@ -5,6 +5,7 @@ class RequestScheduledTasksHandler:
         if not os.path.isdir('/tmp/strongr'):
             os.mkdir('/tmp/strongr')
 
+        print(os.listdit('/tmp/strongr/'))
         tasks = [f for f in os.listdir('/tmp/strongr/') if os.path.isfile('/tmp/strongr/' + f)].sort()
         print(tasks)
         return tasks
