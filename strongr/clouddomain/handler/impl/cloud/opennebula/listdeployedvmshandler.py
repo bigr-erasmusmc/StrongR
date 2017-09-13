0e5c5b6c16196629f320e7d89483e6bf2de7963c
Thomas Phil <thomas@tphil.nl>
2017-09-13 16:50:38 +0200

    debug in cloud \o/

--- strongr/clouddomain/handler/impl/cloud/opennebula/listdeployedvmshandler.py
+++ strongr/clouddomain/handler/impl/cloud/opennebula/listdeployedvmshandler.py
@@ -5,11 +5,15 @@ import salt.cloud
 class ListDeployedVmsHandler(AbstractListDeployedVmsHandler):
     def __call__(self, command):
         client = salt.cloud.CloudClient('/etc/salt/cloud')
-        names = []
+        names = {}
         rs = client.query()
 
         for provider in list(rs.keys()):
             for location in list(rs[provider].keys()):
                 for machine in list(rs[provider][location].keys()):
-                    print(rs[provider][location][machine])
-        #return names
+                    names[machine] = {
+                        'cores': rs[provider][location][machine]['size']['cpu'],
+                        'ram': rs[provider][location][machine]['size']['memory'] / 1024
+                    }
+
+        return names
