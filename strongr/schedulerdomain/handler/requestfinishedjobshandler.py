4d0d7f8f1231a4ad046a7a4dcbe8b2b5d3d344b6
Thomas Phil <thomas@tphil.nl>
2017-10-30 14:23:12 +0100

    RM-44 debugging sqlalchemy in cloud

--- strongr/schedulerdomain/handler/requestfinishedjobshandler.py
+++ strongr/schedulerdomain/handler/requestfinishedjobshandler.py
@@ -4,7 +4,7 @@ from strongr.schedulerdomain.model import Job, JobState
 class RequestFinishedJobsHandler(object):
     def __call__(self, query, *args, **kwargs):
         session = strongr.core.gateways.Gateways.sqlalchemy_session()
-        result = session.query(Job).filter_by(Job.state.in_([JobState.FAILED, JobState.FINISHED])).order_by(
+        result = session.query(Job).filter(Job.state.in_([JobState.FAILED, JobState.FINISHED])).order_by(
             Job.job_id).all()
 
         job_ids = []
