b6679b3329527a410087852dc34bd32dbe5c47fb
Thomas Phil <thomas@tphil.nl>
2017-11-02 15:11:51 +0100

    RM-50 Debugging in cloud

--- strongr/schedulerdomain/handler/jobfinishedhandler.py
+++ strongr/schedulerdomain/handler/jobfinishedhandler.py
@@ -6,14 +6,17 @@ class JobFinishedHandler(object):
     def __init__(self, command):
         db = strongr.core.gateways.Gateways.sqlalchemy_session()
         try:
+            jobstate = (JobState.FINISHED if command.retcode == 0 else JobState.FAILED)
             db.query(Job).filter(Job.job_id == command.job_id).update(
                 {
-                    Job.state: (JobState.FINISHED if command.retcode == 0 else JobState.FAILED),
+                    Job.state: jobstate,
                     Job.stdout: command.ret,
                     Job.return_code: command.retcode
                 },
                 synchronize_session='evaluate'
             )
+            from pprint import pprint
+            pprint(command)
             db.commit()
         except:
             db.rollback()
