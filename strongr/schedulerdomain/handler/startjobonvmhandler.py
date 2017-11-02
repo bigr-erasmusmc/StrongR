8e41678de70cdfc1a2f06f04147e0dd714f8f8da
Thomas Phil <thomas@tphil.nl>
2017-11-02 14:14:39 +0100

    RM-50 Debugging in cloud

--- strongr/schedulerdomain/handler/startjobonvmhandler.py
+++ strongr/schedulerdomain/handler/startjobonvmhandler.py
@@ -2,14 +2,14 @@ import strongr
 import strongr.core.domain.schedulerdomain
 import strongr.core.domain.clouddomain
 from strongr.core.gateways import Gateways
-from strongr.schedulerdomain.model import Job
+from strongr.schedulerdomain.model import Job, JobState
 
 
 class StartJobOnVmHandler:
     def __call__(self, command):
         db = strongr.core.gateways.Gateways.sqlalchemy_session()
         try:
-            db.query(Job).filter(Job.job_id == command.job_id).update({Job.vm_id: command.vm_id}, synchronize_session='evaluate')
+            db.query(Job).filter(Job.job_id == command.job_id).update({Job.vm_id: command.vm_id, Job.state: JobState.RUNNING}, synchronize_session='evaluate')
             db.commit()
         except:
             db.rollback()
