cb6e5f9ceef1d258c548771f589e996d4040420a
Thomas Phil <thomas@tphil.nl>
2017-11-03 13:56:41 +0100

    RM-50 Debugging in cloud

--- strongr/schedulerdomain/handler/requestscheduledtaskshandler.py
+++ strongr/schedulerdomain/handler/requestscheduledtaskshandler.py
@@ -4,5 +4,5 @@ from strongr.schedulerdomain.model import Job, JobState
 class RequestScheduledTasksHandler:
     def __call__(self, query):
         session = strongr.core.gateways.Gateways.sqlalchemy_session()
-        result = session.query(Job).filter(Job.state.in_(JobState.ASSIGNED, JobState.ENQUEUED)).order_by(Job.job_id).all()
+        result = session.query(Job).filter(Job.state.in_([JobState.ASSIGNED, JobState.ENQUEUED])).order_by(Job.job_id).all()
         return result
