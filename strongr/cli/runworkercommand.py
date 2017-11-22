ec1870386e5315aac7e2c2af03ae5524156ee5ca
Thomas Phil <thomas@tphil.nl>
2017-11-22 10:25:46 +0100

    RM-46 worker mode enabled

--- strongr/cli/runworkercommand.py
+++ strongr/cli/runworkercommand.py
@@ -1,3 +1,5 @@
+import uuid
+
 from celery.signals import worker_process_init
 
 from strongr.core.domain.schedulerdomain import SchedulerDomain
@@ -34,16 +36,22 @@ class RunWorkerCommand(Command):
         for command in commands:
             strongr.core.Core.command_router().enable_worker_route_for_command(celery, command)
 
+        workername = '%h@{}'.format(uuid.uuid4())
+
         argv = [
             'worker',
             '--loglevel=DEBUG',
-            '-Q=' + ','.join(commands)
+            '-Q=' + ','.join(commands),
+            '--concurrency=1',
+            '-n {}'.format(workername)
         ]
 
+        # crude fix for celery daemonized threads causing salt to fail
+        # https://github.com/celery/celery/issues/1709
         @worker_process_init.connect
         def fix_multiprocessing(**kwargs):
             # don't be a daemon, so we can create new subprocesses
             from multiprocessing import current_process
             current_process().daemon = False
-            
+
         celery.worker_main(argv)
