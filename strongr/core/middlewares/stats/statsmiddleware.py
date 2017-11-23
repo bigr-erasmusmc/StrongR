ac5fb11f0bf677f307b388b87b4db04356dbeaf0
Thomas Phil <thomas@tphil.nl>
2017-11-23 11:02:39 +0100

    RM-46 worker mode enabled

--- strongr/core/middlewares/stats/statsmiddleware.py
+++ strongr/core/middlewares/stats/statsmiddleware.py
@@ -1,11 +1,11 @@
 from cmndr import Middleware
-from strongr.core.gateways import Gateways
+import strongr.core.gateways
 
 class StatsMiddleware(Middleware):
 
     def execute(self, command, next_callable):
         namespace = command.__module__.split('.')[1] + '.' + command.__class__.lower
-        stats = Gateways.stats()
+        stats = strongr.core.gateways.Gateways.stats()
 
         stats.incr(namespace, 1)
         with stats.time(namespace):
